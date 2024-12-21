948b280cd957f6ab8a63f484236fcaf8aca6c789
Adrian Borup <adrian@borup.com>
2024-12-21 14:24:25 +0100

    Simplify macro

--- tests/tester.rs
+++ tests/tester.rs
@@ -2,20 +2,24 @@ macro_rules! eval_and_assert {
     ($name:ident, $src:expr, $expected_out:expr, $expected_err:expr) => {
         #[test]
         fn $name() -> () {
-            let mut stdout = Vec::new();
-            let mut stderr = Vec::new();
-
-            let interpreter =
-                linefeed::interpreter::Interpreter::new_with_output(&mut stdout, &mut stderr);
-
-            linefeed::run_with_interpreter(interpreter, $src);
-            let stdout_str = std::str::from_utf8(&stdout).unwrap();
-            let stderr_str = std::str::from_utf8(&stderr).unwrap();
-
-            assert_eq_normalised("stderr", stderr_str, $expected_err);
-            assert_eq_normalised("stdout", stdout_str, $expected_out);
+            let (stdout_str, stderr_str) = run_program($src);
+            assert_eq_normalised("stderr", &stderr_str, $expected_err);
+            assert_eq_normalised("stdout", &stdout_str, $expected_out);
         }
     };
+}
+
+fn run_program(src: &str) -> (String, String) {
+    let mut stdout = Vec::new();
+    let mut stderr = Vec::new();
+
+    let interpreter = linefeed::interpreter::Interpreter::new_with_output(&mut stdout, &mut stderr);
+
+    linefeed::run_with_interpreter(interpreter, src);
+    let stdout_str = std::str::from_utf8(&stdout).unwrap().to_string();
+    let stderr_str = std::str::from_utf8(&stderr).unwrap().to_string();
+
+    (stdout_str, stderr_str)
 }
 
 fn assert_eq_normalised(name: &str, expected: &str, actual: &str) {
