40cd2b6933fcd5ad30aa289ea5dc9edfec542730
Adrian Borup <adrian@borup.com>
2025-01-08 14:27:52 +0100

    Read AoC input from stdin

--- tests/linefeed/advent_of_code_2020/mod.rs
+++ tests/linefeed/advent_of_code_2020/mod.rs
@@ -8,6 +8,7 @@ use crate::helpers::{
 eval_and_assert!(
     day01,
     include_str!("day01.lf"),
+    include_str!("inputs/day01.txt"),
     equals(indoc! {r#"
         Part 1: 514579
         Part 2: 241861950
