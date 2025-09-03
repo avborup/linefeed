6edbb80cbd8f3b0a241f18dcb28331369b531955
Adrian Borup <adrian@borup.com>
2025-09-03 09:53:39 +0200

    Add more tests for indexing

--- linefeed/tests/linefeed/list.rs
+++ linefeed/tests/linefeed/list.rs
@@ -38,3 +38,32 @@ eval_and_assert!(
     empty(),
     contains("Method append expects 1 arguments, but got 0")
 );
+
+eval_and_assert!(
+    index_assign_works,
+    indoc! {r#"
+        foo = [1, 2, 3];
+        foo[1] = "new";
+        print(foo);
+    "#},
+    equals("[1, \"new\", 3]"),
+    empty()
+);
+
+eval_and_assert!(
+    index_assign_works_with_inline_expr,
+    indoc! {r#"
+        foo = [1, 2, 3];
+        foo[foo.len() / 2] = "mellon";
+        print(foo);
+
+        foo = ["first", "second", "third"];
+        foo[foo.len() - foo.len() - 1] = foo[1] + "-mellon";
+        print(foo);
+    "#},
+    equals(indoc! {r#"
+        [1, "mellon", 3]
+        ["first", "second", "second-mellon"]
+    "#}),
+    empty()
+);
