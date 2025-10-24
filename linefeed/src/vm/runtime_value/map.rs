aedefa3c29fb98cdf5566656fe70891729f32668
Adrian Borup <adrian@borup.com>
2025-10-24 14:51:29 +0200

    Implement default maps

--- linefeed/src/vm/runtime_value/map.rs
+++ linefeed/src/vm/runtime_value/map.rs
@@ -51,15 +51,23 @@ impl RuntimeMap {
     }
 
     pub fn deep_clone(&self) -> Self {
-        Self::from_map(
+        let new_map = Self::from_map(
             self.borrow()
                 .iter()
                 .map(|(k, v)| (k.deep_clone(), v.deep_clone()))
                 .collect(),
-        )
+        );
+
+        if let Some(default_value) = &self.borrow().default_value {
+            new_map.borrow_mut().default_value = Some(default_value.deep_clone());
+        }
+
+        new_map
     }
 
     pub fn get(&self, key: &RuntimeValue) -> RuntimeValue {
+        self.insert_default_value_if_missing(key);
+
         self.borrow()
             .get(key)
             .cloned()
@@ -72,6 +80,16 @@ impl RuntimeMap {
 
     pub fn contains_key(&self, key: &RuntimeValue) -> bool {
         self.borrow().contains_key(key)
+    }
+
+    fn insert_default_value_if_missing(&self, key: &RuntimeValue) {
+        let Some(default_value) = self.borrow().default_value.clone() else {
+            return;
+        };
+
+        if !self.borrow().contains_key(key) {
+            self.insert(key.clone(), default_value.deep_clone());
+        }
     }
 }
 
