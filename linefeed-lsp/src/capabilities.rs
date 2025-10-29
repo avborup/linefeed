45bcb5ed098fe1f4dc94a893acd79ecb2fb16759
Adrian Borup <adrian@borup.com>
2025-10-29 10:10:00 +0100

    De-duplicate dead code annotations

--- linefeed-lsp/src/capabilities.rs
+++ linefeed-lsp/src/capabilities.rs
@@ -43,57 +43,41 @@ pub const LEGEND_MODIFIERS: &[SemanticTokenModifier] = &[
     SemanticTokenModifier::DEFAULT_LIBRARY,
 ];
 
+pub use token_consts::*;
+
 /// Token type indices corresponding to positions in LEGEND_TYPE
 /// These constants should be used instead of magic numbers
-/// Note: Some constants are unused in the current lexer-based implementation
-/// but will be used when AST-based semantic tokens are added
 #[allow(dead_code)]
-pub const TOKEN_TYPE_NAMESPACE: u32 = 0;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_TYPE: u32 = 1;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_CLASS: u32 = 2;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_ENUM: u32 = 3;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_INTERFACE: u32 = 4;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_STRUCT: u32 = 5;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_TYPE_PARAMETER: u32 = 6;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_PARAMETER: u32 = 7;
-pub const TOKEN_TYPE_VARIABLE: u32 = 8;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_PROPERTY: u32 = 9;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_ENUM_MEMBER: u32 = 10;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_EVENT: u32 = 11;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_FUNCTION: u32 = 12;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_METHOD: u32 = 13;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_MACRO: u32 = 14;
-pub const TOKEN_TYPE_KEYWORD: u32 = 15;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_MODIFIER: u32 = 16;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_COMMENT: u32 = 17;
-pub const TOKEN_TYPE_STRING: u32 = 18;
-pub const TOKEN_TYPE_NUMBER: u32 = 19;
-pub const TOKEN_TYPE_REGEXP: u32 = 20;
-pub const TOKEN_TYPE_OPERATOR: u32 = 21;
-#[allow(dead_code)]
-pub const TOKEN_TYPE_DECORATOR: u32 = 22;
+mod token_consts {
+    pub const TOKEN_TYPE_NAMESPACE: u32 = 0;
+    pub const TOKEN_TYPE_TYPE: u32 = 1;
+    pub const TOKEN_TYPE_CLASS: u32 = 2;
+    pub const TOKEN_TYPE_ENUM: u32 = 3;
+    pub const TOKEN_TYPE_INTERFACE: u32 = 4;
+    pub const TOKEN_TYPE_STRUCT: u32 = 5;
+    pub const TOKEN_TYPE_TYPE_PARAMETER: u32 = 6;
+    pub const TOKEN_TYPE_PARAMETER: u32 = 7;
+    pub const TOKEN_TYPE_VARIABLE: u32 = 8;
+    pub const TOKEN_TYPE_PROPERTY: u32 = 9;
+    pub const TOKEN_TYPE_ENUM_MEMBER: u32 = 10;
+    pub const TOKEN_TYPE_EVENT: u32 = 11;
+    pub const TOKEN_TYPE_FUNCTION: u32 = 12;
+    pub const TOKEN_TYPE_METHOD: u32 = 13;
+    pub const TOKEN_TYPE_MACRO: u32 = 14;
+    pub const TOKEN_TYPE_KEYWORD: u32 = 15;
+    pub const TOKEN_TYPE_MODIFIER: u32 = 16;
+    pub const TOKEN_TYPE_COMMENT: u32 = 17;
+    pub const TOKEN_TYPE_STRING: u32 = 18;
+    pub const TOKEN_TYPE_NUMBER: u32 = 19;
+    pub const TOKEN_TYPE_REGEXP: u32 = 20;
+    pub const TOKEN_TYPE_OPERATOR: u32 = 21;
+    pub const TOKEN_TYPE_DECORATOR: u32 = 22;
+}
 
 /// Build the server capabilities for initialization
 pub fn build_server_capabilities() -> ServerCapabilities {
     ServerCapabilities {
-        text_document_sync: Some(TextDocumentSyncCapability::Kind(
-            TextDocumentSyncKind::FULL,
-        )),
+        text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
         semantic_tokens_provider: Some(
             SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
                 SemanticTokensRegistrationOptions {
