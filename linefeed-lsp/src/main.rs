718081c59552f89da76f6f14c9b5bf6a444f2aee
Adrian Borup <adrian@borup.com>
2025-10-29 09:22:26 +0100

    Add non-working LSP code

--- linefeed-lsp/src/main.rs
+++ linefeed-lsp/src/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use linefeed::chumsky::Parser as _;
+use tokio::sync::Mutex;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
@@ -5,12 +9,79 @@ use tower_lsp::{Client, LanguageServer, LspService, Server};
 #[derive(Debug)]
 struct Backend {
     client: Client,
+    sources: Mutex<HashMap<String, String>>,
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
+        const LEGEND_TYPE: &[SemanticTokenType] = &[
+            SemanticTokenType::FUNCTION,
+            SemanticTokenType::VARIABLE,
+            SemanticTokenType::STRING,
+            SemanticTokenType::COMMENT,
+            SemanticTokenType::NUMBER,
+            SemanticTokenType::KEYWORD,
+            SemanticTokenType::OPERATOR,
+            SemanticTokenType::PARAMETER,
+        ];
+
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities {
+                inlay_hint_provider: Some(OneOf::Left(true)),
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: Some(false),
+                    trigger_characters: Some(vec![".".to_string()]),
+                    work_done_progress_options: Default::default(),
+                    all_commit_characters: None,
+                    completion_item: None,
+                }),
+                execute_command_provider: Some(ExecuteCommandOptions {
+                    commands: vec!["olalal.orale".to_string()],
+                    work_done_progress_options: Default::default(),
+                }),
+                workspace: Some(WorkspaceServerCapabilities {
+                    workspace_folders: Some(WorkspaceFoldersServerCapabilities {
+                        supported: Some(true),
+                        change_notifications: Some(OneOf::Left(true)),
+                    }),
+                    file_operations: None,
+                }),
+                semantic_tokens_provider: Some(
+                    SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
+                        SemanticTokensRegistrationOptions {
+                            text_document_registration_options: {
+                                TextDocumentRegistrationOptions {
+                                    document_selector: Some(vec![DocumentFilter {
+                                        language: Some("lf".to_string()),
+                                        scheme: Some("file".to_string()),
+                                        pattern: None,
+                                    }]),
+                                }
+                            },
+                            semantic_tokens_options: SemanticTokensOptions {
+                                work_done_progress_options: WorkDoneProgressOptions::default(),
+                                legend: SemanticTokensLegend {
+                                    token_types: LEGEND_TYPE.into(),
+                                    token_modifiers: vec![],
+                                },
+                                range: Some(true),
+                                full: Some(SemanticTokensFullOptions::Bool(true)),
+                            },
+                            static_registration_options: StaticRegistrationOptions::default(),
+                        },
+                    ),
+                ),
+                definition_provider: Some(OneOf::Left(true)),
+                references_provider: Some(OneOf::Left(true)),
+                rename_provider: Some(OneOf::Left(true)),
+                ..ServerCapabilities::default()
+            },
+        })
     }
 
     async fn initialized(&self, _: InitializedParams) {
@@ -21,6 +92,57 @@ impl LanguageServer for Backend {
 
     async fn shutdown(&self) -> Result<()> {
         Ok(())
+    }
+
+    async fn did_save(&self, _: DidSaveTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "Linefeed file saved!")
+            .await;
+    }
+
+    async fn semantic_tokens_full(
+        &self,
+        params: SemanticTokensParams,
+    ) -> Result<Option<SemanticTokensResult>> {
+        let uri = params.text_document.uri.to_string();
+
+        self.client
+            .log_message(MessageType::LOG, "semantic_token_full")
+            .await;
+
+        let src = self.sources.lock().await.get(&uri).cloned();
+
+        let tokens = match linefeed::grammar::lexer::lexer()
+            .parse(src)
+            .into_output_errors()
+        {
+            (Some(tokens), e) if e.is_empty() => tokens,
+            (_, e) => todo!(),
+        };
+
+        let mut semantic_tokens: Vec<SemanticToken> = vec![];
+
+        for token in tokens {
+            let semantic_token = SemanticToken {
+                delta_line: 1,
+                delta_start: 1,
+                length: 4,
+                token_type: 0,
+                token_modifiers_bitset: 0,
+            };
+
+            semantic_tokens.push(semantic_token);
+
+            Ok(Some(SemanticTokensResult::Tokens(SemanticTokens {
+                result_id: None,
+                data: semantic_tokens,
+            })))
+        }
+
+        // let ast = match linefeed::parse_tokens(src, &tokens) {
+        //     Ok(ast) => ast,
+        //     Err(errs) => todo!(),
+        // };
     }
 }
 
